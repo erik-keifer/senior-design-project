73f2fe65915efafc29374c7050fd57c5bac2ec91
erik-keifer <69150393+erik-keifer@users.noreply.github.com>
2025-12-18 16:37:09 -0800

    Initial commit

---
+++ Senior Design Project/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Senior Design Project
+//
+//  Created by Erik Keifer on 12/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Senior Design Project/Senior_Design_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Senior_Design_ProjectApp.swift
+//  Senior Design Project
+//
+//  Created by Erik Keifer on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Senior_Design_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
